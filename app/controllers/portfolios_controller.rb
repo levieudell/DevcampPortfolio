191e44d7dbf16d1342dc4f918c5e47c9ca61c1ef
levieudell <34821328+levieudell@users.noreply.github.com>
2018-02-04 11:49:38 -0800

    implemented show functianality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,7 +26,7 @@ class PortfoliosController < ApplicationController
    
     def update
         @portfolio_item = Portfolio.find(params[:id])
-        
+
         respond_to do |format|
           if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
             format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }
@@ -36,7 +36,12 @@ class PortfoliosController < ApplicationController
             format.json { render json: @blog.errors, status: :unprocessable_entity }
           end
         end
-      end
+    end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+    
 
 
 end
