0a17e13250628ef52c98bca0d434477734624f09
levieudell <34821328+levieudell@users.noreply.github.com>
2018-02-04 09:33:52 -0800

    integrated index action for portolio items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
-<<<<<<< HEAD
     @skills = Skill.all
-=======
->>>>>>> 4979f1b3d61cdea0d03f413e623a3ee14ad33639
   end
 
   def about
